600d0200a7a3c14c75eb82b530c52a822fbf495e
taekyung Kim <96978276+doum6792@users.noreply.github.com>
2022-01-02 22:06:31 +0900

    Delete html/html directory

--- html/html/mcdonald/jquery/mcdonald1.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".main > li, .sub, .bg").mouseover(function(){
-        $(".sub, .bg").stop().slideDown()
-    })
-    $(".main > li, .sub, .bg").mouseleave(function(){
-        $(".sub, .bg").stop().slideUp();
-    })
-  })
